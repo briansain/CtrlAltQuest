5e36424c28b3e1841cd9837a91887745fabf497a
briansain <brian.sain@yahoo.com>
2024-11-07 08:57:06 -0600

    add pdfs for stat reference; make more updates to character sheet UI

--- src/CtrlAltQuest.Pathfinder2e/Actors/CharacterState.cs
+++ src/CtrlAltQuest.Pathfinder2e/Actors/CharacterState.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 using CtrlAltQuest.Pathfinder2e.Ancestry;
 
 namespace CtrlAltQuest.Pathfinder2e.Actors;
@@ -6,8 +8,18 @@ namespace CtrlAltQuest.Pathfinder2e.Actors;
 public class CharacterState
 {
     public string Id { get; private set; }
-    public IAncestry? Ancestry { get; private set;}
+    public string Name { get; private set; }
+    public int Level { get; private set; }
+    public string AncestryName { get; private set;}
+    public string BackgroundName { get; private set; }
+    public string ClassName { get; private set; }
     public int MaxHitPoints { get; private set; }
+    public int Strength { get; private set; }
+    public int Dexterity { get; private set; }
+    public int Constitution { get; private set; }
+    public int Intelligence { get; private set; }
+    public int Wisdom { get; private set; }
+    public int Charisma { get; private set; }
     public Size Size { get; private set; }
     public List<Ability> Abilities { get; private set; } = new List<Ability>();
     public List<Trait> Traits { get; private set; } = new List<Trait>();
@@ -16,11 +28,24 @@ public class CharacterState
     public CharacterState(string id)
     {
         Id = id;
+        Name = "Bonesaw";
+        Level = 1;
+        AncestryName = "Human";
+        BackgroundName = "Hunter";
+        ClassName = "Fighter";
+        MaxHitPoints = 20;
+        Strength = 4;
+        Dexterity = 2;
+        Constitution = 2;
+        Intelligence = 0;
+        Wisdom = 1;
+        Charisma = 0;
     }
     internal void AssignAncestry(string ancestryName)
     {
+        // add a safety check for reference not existing
         var ancestry = AncestryReferences.AllAncestries.Where(a => a.Name == ancestryName).Single();
-        Ancestry = ancestry;
+        AncestryName = ancestryName;
         MaxHitPoints += ancestry.BaseHitPoints;
         Size = ancestry.Size;
         Abilities.AddRange(ancestry.Abilities);
