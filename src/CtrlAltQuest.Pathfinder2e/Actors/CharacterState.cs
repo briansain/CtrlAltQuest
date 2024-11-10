933f13b9713e00560525c5ebf3f78de8ffd41c90
briansain <brian.sain@yahoo.com>
2024-11-09 19:29:28 -0600

    add svgs for stats; add attribution file

--- src/CtrlAltQuest.Pathfinder2e/Actors/CharacterState.cs
+++ src/CtrlAltQuest.Pathfinder2e/Actors/CharacterState.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Security.Policy;
 using CtrlAltQuest.Pathfinder2e.Ancestry;
 
 namespace CtrlAltQuest.Pathfinder2e.Actors;
@@ -10,7 +11,7 @@ public class CharacterState
     public string Id { get; private set; }
     public string Name { get; private set; }
     public int Level { get; private set; }
-    public string AncestryName { get; private set;}
+    public string AncestryName { get; private set; }
     public string BackgroundName { get; private set; }
     public string ClassName { get; private set; }
     public int MaxHitPoints { get; private set; }
@@ -21,6 +22,14 @@ public class CharacterState
     public int Wisdom { get; private set; }
     public int Charisma { get; private set; }
     public Size Size { get; private set; }
+    public int ArmorClass { get; private set; }
+    public int Shield { get; private set; }
+    public int Speed { get; private set; }
+    public int Perception { get; private set; }
+    public Proficiency UnarmoredProficiency { get; private set; }
+    public Proficiency LightArmorProficiency { get; private set; }
+    public Proficiency MediumArmorProficiency { get; private set; }
+    public Proficiency HeavyArmorProficiency { get; private set; }
     public List<Ability> Abilities { get; private set; } = new List<Ability>();
     public List<Trait> Traits { get; private set; } = new List<Trait>();
     public List<string> Languages { get; private set; } = new List<string>();
@@ -40,7 +49,16 @@ public class CharacterState
         Intelligence = 0;
         Wisdom = 1;
         Charisma = 0;
+        ArmorClass = 18;
+        Shield = 2;
+        Size = Size.Medium;
+        Speed = 25;
+        UnarmoredProficiency = Proficiency.Trained;
+        LightArmorProficiency = Proficiency.Trained;
+        MediumArmorProficiency = Proficiency.Trained;
+        HeavyArmorProficiency = Proficiency.Trained;
     }
+
     internal void AssignAncestry(string ancestryName)
     {
         // add a safety check for reference not existing
@@ -52,4 +70,21 @@ public class CharacterState
         Traits.AddRange(ancestry.Traits);
         Languages.AddRange(ancestry.BaseLanguages);
     }
+
+    internal void SetProficiencies(Proficiency unarmored, Proficiency light, Proficiency medium, Proficiency heavy)
+    {
+        UnarmoredProficiency = unarmored;
+        LightArmorProficiency = light;
+        MediumArmorProficiency = medium;
+        HeavyArmorProficiency = heavy;
+    }
+}
+
+public enum Proficiency
+{
+    Untrained,
+    Trained,
+    Expert,
+    Master,
+    Legendary
 }
