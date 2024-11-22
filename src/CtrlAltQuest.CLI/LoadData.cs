9a6b1ff9f204487e6ca6184962338b2729a11215
briansain <brian.sain@yahoo.com>
2024-11-21 21:14:57 -0600

    add redis and cli

---
+++ src/CtrlAltQuest.CLI/LoadData.cs
@@ -0,0 +1,51 @@
+﻿using CtrlAltQuest.Pathfinder2e.Actors;
+using Redis.OM;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Akka.Actor.ProviderSelection;
+
+namespace CtrlAltQuest.CLI
+{
+    internal class LoadData
+    {
+        private string _fileLocation { get; set; }
+        public LoadData(string fileLocation)
+        {
+            _fileLocation = fileLocation;
+        }
+
+        public async Task Start()
+        {
+            var r = ConnectionMultiplexer.Connect("localhost:6379");
+            
+            var redis = new RedisConnectionProvider(r);
+
+            var definition = redis.Connection.GetIndexInfo(typeof(CharacterState));
+            var i = redis.Connection.GetIndexInfo(typeof(CharacterState));
+            if (i == null)
+            {
+                redis.Connection.DropIndex(typeof(CharacterState));
+                redis.Connection.CreateIndex(typeof(CharacterState));
+            }
+            if (!File.Exists(_fileLocation))
+            {
+                throw new Exception($"Could not find file at location {_fileLocation}");
+            }
+
+            var characterState = redis.RedisCollection<CharacterState>();
+            if (await characterState.AnyAsync(c => c.Id == "bonesaw3"))
+            {
+                Console.WriteLine("Character already exists");
+            }
+            else
+            {
+                Console.WriteLine("Character does not already exist");
+                await characterState.InsertAsync(new CharacterState("bonesaw3"));
+            }
+        }
+    }
+}
---
+++ src/CtrlAltQuest.CLI/Program.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using CtrlAltQuest.CLI;
+
+Parser.Default.ParseArguments<Options>(args)
+.WithParsed(o =>
+{
+    if (o.LoadData)
+    {
+        Console.WriteLine($"Starting to load data from {o.FileLocation}");
+        var loadData = new LoadData(o.FileLocation);
+        loadData.Start().Wait();
+    }
+    else
+    {
+        Console.WriteLine($"Current Arguments: -v {o.LoadData}");
+        Console.WriteLine("Quick Start Example!");
+    }
+    Console.WriteLine("END");
+});
+
+public class Options
+{
+    [Option('l', "loadData", Required = false, HelpText = "Loads Data into Redis")]
+    public bool LoadData { get; set; }
+    [Option("fileLocation")]
+    public string FileLocation { get; set; } = string.Empty;
+}
